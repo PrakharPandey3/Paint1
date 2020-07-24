50ffdc8ad2e84a373bee1310f1ffc092f4e2755d
PrakharPandey3 <63103704+PrakharPandey3@users.noreply.github.com>
2020-07-24 17:51:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var canvas;
+var drawing = [];
+var currentPath = [];
+var isDrawing = false;
+var database;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  canvas.mousePressed(startPath);
+  canvas.parent('canvascontainer');
+  canvas.mouseReleased(endPath);
+ var saveButton = select('#saveButton');
+ saveButton.mousePressed(saveDrawing);
+ database = firebase.database();
+}
+
+function startPath(){
+  isDrawing = true;
+  currentPath = [];
+  drawing.push(currentPath);
+}
+
+function endPath(){
+ isDrawing = false;
+}
+
+function draw(){
+ 
+  background(0)
+
+  if(isDrawing){
+    var point = {
+      x: mouseX,
+      y: mouseY
+    }
+    currentPath.push(point);
+  }
+
+stroke(255);
+strokeWeight(4);
+noFill();
+for(var i = 0; i < drawing.length; i++){
+  var path = drawing[i];
+  beginShape();
+  for(var j = 0; j < path.length; j++){
+    vertex(path[j].x, path[j].y)
+  }
+  endShape();
+}
+
+}
+
+function saveDrawing(){
+  var ref = database.ref('drawings');
+  var data = {
+    name: "Prakhar",
+    drawing: drawing
+  }
+  var result = ref.push(data, dataSent);
+  console.log(result.key);
+
+  function dataSent(status){
+    console.log(status);
+  }
+}
